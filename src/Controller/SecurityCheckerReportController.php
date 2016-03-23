0ea749c9d6f7ce77de7a74eb85d7f850d56a5485
Matt Chapman <chapabu101@gmail.com>
2016-03-23 23:35:17 +0000

    Added link to security notice link.

--- src/Controller/SecurityCheckerReportController.php
+++ src/Controller/SecurityCheckerReportController.php
@@ -7,9 +7,13 @@
 
 namespace Drupal\composer_security_checker\Controller;
 
+use Drupal\Component\Utility\UrlHelper;
 use Drupal\composer_security_checker\Collections\AdvisoryCollection;
+use Drupal\composer_security_checker\Models\Advisory;
 use Drupal\composer_security_checker\Repositories\RepositoryInterface;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Drupal\Core\Utility\LinkGenerator;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -27,13 +31,23 @@ class SecurityCheckerReportController extends ControllerBase {
   protected $composerSecurityRepository;
 
   /**
+   * A LinkGenerator instance.
+   *
+   * @var LinkGenerator
+   */
+  protected $linkGenerator;
+
+  /**
    * SecurityCheckerReportController constructor.
    *
    * @param RepositoryInterface $composer_security_repository
    *   A security repository to collect security advisories.
+   * @param LinkGenerator $link_generator
+   *   A LinkGenerator instance.
    */
-  public function __construct(RepositoryInterface $composer_security_repository) {
+  public function __construct(RepositoryInterface $composer_security_repository, LinkGenerator $link_generator) {
     $this->composerSecurityRepository = $composer_security_repository;
+    $this->linkGenerator = $link_generator;
   }
 
   /**
@@ -68,7 +82,7 @@ class SecurityCheckerReportController extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('composer_security_checker.security_checker_repository'),
-      $container->get('string_translation')
+      $container->get('link_generator')
     );
   }
 
@@ -112,18 +126,45 @@ class SecurityCheckerReportController extends ControllerBase {
   private function getRows(AdvisoryCollection $advisories) {
     $rows = [];
 
-    /** @var \Drupal\composer_security_checker\Models\Advisory $advisory */
+    /** @var Advisory $advisory */
     foreach ($advisories as $advisory) {
+
       $rows[] = [
         $advisory->getLibraryName(),
         $advisory->getLibraryVersion(),
         $advisory->getAdvisoryIdentifier(),
         $advisory->getAdvisoryTitle(),
-        $advisory->getAdvisoryLink(),
+        $this->getAdvisoryLinkMarkup($advisory),
       ];
+
     }
 
     return $rows;
+  }
+
+  /**
+   * Get an actual link to the security advisory.
+   *
+   * @param Advisory $advisory
+   *   A security advisory instance. We could just pass the link tetx in here,
+   *   but I'd rather be able to typehint it.
+   *
+   * @return \Drupal\Core\GeneratedLink
+   *   A safe and generated link.
+   */
+  private function getAdvisoryLinkMarkup(Advisory $advisory) {
+
+    // We can be pretty sure the URL is safe, but we should check anyway.
+    $unsafe_url = $advisory->getAdvisoryLink();
+    $safe_url = UrlHelper::stripDangerousProtocols($unsafe_url);
+
+    // Build the URL.
+    $url = Url::fromUri($safe_url);
+    $url->setOption('external', TRUE);
+    $url->setOption('attributes', ['target' => '_blank']);
+
+    return $this->linkGenerator->generate($url->getUri(), $url);
+
   }
 
 }
